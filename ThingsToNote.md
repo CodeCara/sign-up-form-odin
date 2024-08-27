19fef5a1fe5ff306cbdcbd991eb742abbd3b624c
CodeCara <149534506+CodeCara@users.noreply.github.com>
2024-08-27 14:48:54 +0200

    General changes committed (forgot to commit earlier, logging to have a back-up)

--- ThingsToNote.md
+++ ThingsToNote.md
@@ -1,1 +1,4 @@
 Credit image author - planet, Daniel Olah Unsplash!
+//26 aug 24...need to target after pseudo element with JS
+
+//console.log(cssOfMessage.content); is displaying content as 'none', don't know why?
---
+++ script.js
@@ -0,0 +1,7 @@
+let password = document.getElementById('password');
+let confirmPassword = document.getElementById('confirm-password');
+let button = document.getElementById('button');
+
+let cssOfMessage = window.getComputedStyle(password, '::after');
+
+//!!!!!Code below works but not possible to dynamically change the visibility of cssOfMessage!!!!
